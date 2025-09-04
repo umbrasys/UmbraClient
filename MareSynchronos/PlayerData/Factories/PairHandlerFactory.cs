4495177f02153cdd5a978820dff219e6d3a11ba6
SirConstance <sirconstance@pm.me>
2025-09-04 21:22:39 +0200

    Update 0.0.5 - Change repo & services

--- MareSynchronos/PlayerData/Factories/PairHandlerFactory.cs
+++ MareSynchronos/PlayerData/Factories/PairHandlerFactory.cs
@@ -1,60 +1,33 @@
-﻿using MareSynchronos.FileCache;
-using MareSynchronos.Interop.Ipc;
+﻿using MareSynchronos.API.Data;
+using MareSynchronos.API.Dto.Group;
 using MareSynchronos.MareConfiguration;
-using MareSynchronos.PlayerData.Handlers;
 using MareSynchronos.PlayerData.Pairs;
-using MareSynchronos.Services;
 using MareSynchronos.Services.Mediator;
 using MareSynchronos.Services.ServerConfiguration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace MareSynchronos.PlayerData.Factories;
 
-public class PairHandlerFactory
+public class PairFactory
 {
-    private readonly MareConfigService _configService;
-    private readonly DalamudUtilService _dalamudUtilService;
-    private readonly FileCacheManager _fileCacheManager;
-    private readonly FileDownloadManagerFactory _fileDownloadManagerFactory;
-    private readonly GameObjectHandlerFactory _gameObjectHandlerFactory;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-    private readonly IpcManager _ipcManager;
+    private readonly PairHandlerFactory _cachedPlayerFactory;
     private readonly ILoggerFactory _loggerFactory;
     private readonly MareMediator _mareMediator;
-    private readonly PlayerPerformanceService _playerPerformanceService;
-    private readonly ServerConfigurationManager _serverConfigManager;
-    private readonly PluginWarningNotificationService _pluginWarningNotificationManager;
-    private readonly PairAnalyzerFactory _pairAnalyzerFactory;
-    private readonly VisibilityService _visibilityService;
+    private readonly MareConfigService _mareConfig;
+    private readonly ServerConfigurationManager _serverConfigurationManager;
 
-    public PairHandlerFactory(ILoggerFactory loggerFactory, GameObjectHandlerFactory gameObjectHandlerFactory, IpcManager ipcManager,
-        FileDownloadManagerFactory fileDownloadManagerFactory, DalamudUtilService dalamudUtilService,
-        PluginWarningNotificationService pluginWarningNotificationManager, IHostApplicationLifetime hostApplicationLifetime,
-        FileCacheManager fileCacheManager, MareMediator mareMediator, PlayerPerformanceService playerPerformanceService,
-        ServerConfigurationManager serverConfigManager, PairAnalyzerFactory pairAnalyzerFactory,
-        MareConfigService configService, VisibilityService visibilityService)
+    public PairFactory(ILoggerFactory loggerFactory, PairHandlerFactory cachedPlayerFactory,
+        MareMediator mareMediator, MareConfigService mareConfig, ServerConfigurationManager serverConfigurationManager)
     {
         _loggerFactory = loggerFactory;
-        _gameObjectHandlerFactory = gameObjectHandlerFactory;
-        _ipcManager = ipcManager;
-        _fileDownloadManagerFactory = fileDownloadManagerFactory;
-        _dalamudUtilService = dalamudUtilService;
-        _pluginWarningNotificationManager = pluginWarningNotificationManager;
-        _hostApplicationLifetime = hostApplicationLifetime;
-        _fileCacheManager = fileCacheManager;
+        _cachedPlayerFactory = cachedPlayerFactory;
         _mareMediator = mareMediator;
-        _playerPerformanceService = playerPerformanceService;
-        _serverConfigManager = serverConfigManager;
-        _pairAnalyzerFactory = pairAnalyzerFactory;
-        _configService = configService;
-        _visibilityService = visibilityService;
+        _mareConfig = mareConfig;
+        _serverConfigurationManager = serverConfigurationManager;
     }
 
-    public PairHandler Create(Pair pair)
+    public Pair Create(UserData userData)
     {
-        return new PairHandler(_loggerFactory.CreateLogger<PairHandler>(), pair, _pairAnalyzerFactory.Create(pair), _gameObjectHandlerFactory,
-            _ipcManager, _fileDownloadManagerFactory.Create(), _pluginWarningNotificationManager, _dalamudUtilService, _hostApplicationLifetime,
-            _fileCacheManager, _mareMediator, _playerPerformanceService, _serverConfigManager, _configService, _visibilityService);
+        return new Pair(_loggerFactory.CreateLogger<Pair>(), userData, _cachedPlayerFactory, _mareMediator, _mareConfig, _serverConfigurationManager);
     }
 }
--- MareSynchronos/Plugin.cs
+++ MareSynchronos/Plugin.cs
@@ -29,7 +29,7 @@ using MareSynchronos.Services.CharaData;
 
 using MareSynchronos;
 
-namespace Umbra;
+namespace Snowcloak;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -40,7 +40,7 @@ public sealed class Plugin : IDalamudPlugin
 #pragma warning restore CA2211, CS8618, MA0069, S1104, S2223
     public Action<IFramework>? RealOnFrameworkUpdate { get; set; }
 
-    // Proxy function in the UmbraSync namespace to avoid confusion in /xlstats
+    // Proxy function in the SnowcloakSync namespace to avoid confusion in /xlstats
     public void OnFrameworkUpdate(IFramework framework)
     {
         RealOnFrameworkUpdate?.Invoke(framework);
