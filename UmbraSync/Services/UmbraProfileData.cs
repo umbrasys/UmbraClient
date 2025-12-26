339a068bbbb2d1ba1d10c8312561a625eb5daf57
Keda <sirconstance@pm.me>
2025-12-26 17:45:27 +0100

    Update API pour Profil RP

---
+++ UmbraSync/Services/UmbraProfileData.cs
@@ -0,0 +1,11 @@
+﻿namespace UmbraSync.Services;
+
+public record UmbraProfileData(bool IsFlagged, bool IsNSFW, string Base64ProfilePicture, string Description,
+    string? Base64RpProfilePicture = null, string? RpDescription = null, bool IsRpNSFW = false,
+    string? RpFirstName = null, string? RpLastName = null, string? RpTitle = null, string? RpAge = null,
+    string? RpHeight = null, string? RpBuild = null, string? RpOccupation = null, string? RpAffiliation = null,
+    string? RpAlignment = null, string? RpAdditionalInfo = null)
+{
+    public Lazy<byte[]> ImageData { get; } = new Lazy<byte[]>(() => string.IsNullOrEmpty(Base64ProfilePicture) ? Array.Empty<byte>() : Convert.FromBase64String(Base64ProfilePicture));
+    public Lazy<byte[]> RpImageData { get; } = new Lazy<byte[]>(() => string.IsNullOrEmpty(Base64RpProfilePicture) ? Array.Empty<byte>() : Convert.FromBase64String(Base64RpProfilePicture));
+}
---
+++ UmbraSync/Services/UmbraProfileManager.cs
@@ -0,0 +1,82 @@
+﻿using UmbraSync.API.Data;
+using UmbraSync.API.Data.Comparer;
+using UmbraSync.MareConfiguration;
+using UmbraSync.Services.Mediator;
+using UmbraSync.WebAPI;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using API = UmbraSync.API;
+
+namespace UmbraSync.Services;
+
+public class UmbraProfileManager : MediatorSubscriberBase
+{
+    private const string _noDescription = "-- User has no description set --";
+    private const string _nsfw = "Profile not displayed - NSFW";
+    private readonly ApiController _apiController;
+    private readonly MareConfigService _mareConfigService;
+    private readonly RpConfigService _rpConfigService;
+    private readonly ConcurrentDictionary<UserData, UmbraProfileData> _umbraProfiles = new(UserDataComparer.Instance);
+
+    private readonly UmbraProfileData _defaultProfileData = new(IsFlagged: false, IsNSFW: false, string.Empty, _noDescription);
+    private readonly UmbraProfileData _loadingProfileData = new(IsFlagged: false, IsNSFW: false, string.Empty, "Loading Data from server...");
+    private readonly UmbraProfileData _nsfwProfileData = new(IsFlagged: false, IsNSFW: false, string.Empty, _nsfw);
+
+    public UmbraProfileManager(ILogger<UmbraProfileManager> logger, MareConfigService mareConfigService,
+        RpConfigService rpConfigService, MareMediator mediator, ApiController apiController) : base(logger, mediator)
+    {
+        _mareConfigService = mareConfigService;
+        _rpConfigService = rpConfigService;
+        _apiController = apiController;
+
+        Mediator.Subscribe<ClearProfileDataMessage>(this, (msg) =>
+        {
+            if (msg.UserData != null)
+                _umbraProfiles.TryRemove(msg.UserData, out _);
+            else
+                _umbraProfiles.Clear();
+        });
+        Mediator.Subscribe<DisconnectedMessage>(this, (_) => _umbraProfiles.Clear());
+    }
+
+    public UmbraProfileData GetUmbraProfile(UserData data)
+    {
+        if (!_umbraProfiles.TryGetValue(data, out var profile))
+        {
+            _ = Task.Run(() => GetUmbraProfileFromService(data));
+            return (_loadingProfileData);
+        }
+
+        return (profile);
+    }
+
+    private async Task GetUmbraProfileFromService(UserData data)
+    {
+        try
+        {
+            _umbraProfiles[data] = _loadingProfileData;
+            var profile = await _apiController.UserGetProfile(new API.Dto.User.UserDto(data)).ConfigureAwait(false);
+            UmbraProfileData profileData = new(profile.Disabled, profile.IsNSFW ?? false,
+                string.IsNullOrEmpty(profile.ProfilePictureBase64) ? string.Empty : profile.ProfilePictureBase64,
+                string.IsNullOrEmpty(profile.Description) ? _noDescription : profile.Description,
+                profile.RpProfilePictureBase64, profile.RpDescription, profile.IsRpNSFW ?? false,
+                profile.RpFirstName, profile.RpLastName, profile.RpTitle, profile.RpAge,
+                profile.RpHeight, profile.RpBuild, profile.RpOccupation, profile.RpAffiliation,
+                profile.RpAlignment, profile.RpAdditionalInfo);
+            if (profileData.IsNSFW && !_mareConfigService.Current.ProfilesAllowNsfw && !string.Equals(_apiController.UID, data.UID, StringComparison.Ordinal))
+            {
+                _umbraProfiles[data] = _nsfwProfileData;
+            }
+            else
+            {
+                _umbraProfiles[data] = profileData;
+            }
+        }
+        catch (Exception ex)
+        {
+            // if fails save DefaultProfileData to dict
+            Logger.LogWarning(ex, "Failed to get Profile from service for user {user}", data);
+            _umbraProfiles[data] = _defaultProfileData;
+        }
+    }
+}
